fa61fef307b0c9472247b6723051f610c26066d2
Rubin <rjhand@gmail.com>
2020-08-03 05:04:27 +0000

    Make minor change to environment variable syntax

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase';
+const ENV = process.env;
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: ENV['REACT_APP_FIREBASE_KEY'],
+  authDomain: ENV['REACT_APP_FIREBASE_AUTH_DOMAIN'],
+  databaseURL: ENV['REACT_APP_FIREBASE_DB_URL'],
+  projectId: ENV['REACT_APP_FIREBASE_PROJECT_ID'],
+  storageBucket: ENV['REACT_APP_FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: ENV['REACT_APP_FIREBASE_MESSAGING_SENDER_ID'],
+  appId: ENV['REACT_APP_FIREBASE_APP_ID'],
+  measurementId: ENV['REACT_APP_FIREBASE_MEASUREMENT_ID']
 });
 
 const db = firebaseApp.firestore();
