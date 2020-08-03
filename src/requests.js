9b928ea4152819ec0452cbe039b9f307870dcb14
Rubin <rjhand@gmail.com>
2020-08-03 03:01:33 +0000

    Add environment variables for react

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "7284d09bdec8c2cd1d443406f3b7a35b";
+const API_KEY = process.env.REACT_APP_TEST_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`
