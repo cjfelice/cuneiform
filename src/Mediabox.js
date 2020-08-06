5e55d1c14d45f609bfecc0a2ce88e2ca9b8b3fe2
Christopher Smith <monocularmedia@gmail.com>
2020-08-05 18:15:33 -0700

    solve canvas bounding issues and continue on mediabox component

--- src/Mediabox.js
+++ src/Mediabox.js
@@ -1,14 +1,35 @@
 import React, { useRef, useState } from "react";
 import "./App.scss";
+import Button from "./Button";
+import ReactPlayer from "react-player";
 
-function Mediabox({ text }) {
+function Mediabox(props) {
+  const [mediaURL, setMediaURL] = useState(props.url || "");
+  const [error, setError] = useState("");
+  const [contentType, setContentType] = useState(props.contentType);
+
+  function validate() {
+    if (mediaURL === "") {
+      setError("url cannot be blank");
+      return;
+    }
+    setError("");
+    // onSave(mediaURL);
+  }
+
   return (
-    <img
+    <ReactPlayer
       width="100%"
       height="100%"
-      src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/cc_iStock-478639870_16x9.jpg?itok=1-jMc4Xv"
-      alt="tree"
-    ></img>
+      url="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/cc_iStock-478639870_16x9.jpg?itok=1-jMc4Xv"
+      controls
+      muted
+      config={{
+        youtube: {
+          playerVars: { showinfo: 1 },
+        },
+      }}
+    />
   );
 }
 
