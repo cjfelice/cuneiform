72cf4a0bbc717ffbc72b5f77049688dd8679bddc
Rubin J <rubin@praxis.local>
2020-08-08 09:45:02 -0700

    Reconnect to firebase; Add authentication component to navbar

--- src/App.js
+++ src/App.js
@@ -22,27 +22,7 @@ import UserAuth from './auth/authUser';
 function App() {
   const [mode, setMode] = useState('HOME');
   //sample database inside useState array; sets value to panels
-  const [panels, setPanels] = useState([
-    {
-      username: 'Jasper',
-      title: 'Test1/rename to title!',
-      description: 'Yangmingshan Taipei Chinese Pavilion!',
-      music_id: '',
-      media: [
-        {
-          mediaUrl:
-            'https://p1.pxfuel.com/preview/326/736/1008/people-whimsical-lazy-suit.jpg'
-        },
-        {
-          mediaUrl: 'https://www.youtube.com/watch?v=f7T48W0cwXM&t=6269s'
-        },
-        {
-          mediaUrl:
-            'https://static.pexels.com/photos/8486/water-rain-raindrops-drops.jpg'
-        }
-      ]
-    }
-  ]);
+  const [panels, setPanels] = useState([]);
   /*
   - for the time being, panels and cards will be used interchangeably; cards use material ui, panels were created initially for setup and testing. can now use the functional elements of panels and apply them to cards.
   - hardcoded data set is in dummyData.js if required
@@ -79,9 +59,9 @@ function App() {
       )}
       <Row title='Suggested Canvi' fetchUrl={requests.fetchTrending} />
 
+      {/* {panels.map((panel) => ( */}
       <div className='panels_canvis'>
-        {/* {panels.map(({ id, panel }) => ( */}
-        {panels.map((panel) => (
+        {panels.map(({ id, panel }) => (
           <Panels
             key={id}
             panel_id={id}
